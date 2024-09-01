2c3dcf0d0e84bc26dceef6b718ce71f168bdf315
2a6m3a2n <guptaaman9036@gmail.com>
2024-09-01 13:33:14 +0530

    product minor fixes

--- components/product/product-table/product-table.tsx
+++ components/product/product-table/product-table.tsx
@@ -1,14 +1,9 @@
-"use client"
+"use client";
 
-import { Product } from "@prisma/client"
-import { DataTableProduct } from "./data-table"
-import { columns } from "./column"
-
+import { Product } from "@prisma/client";
+import { DataTableProduct } from "./data-table";
+import { columns } from "./column";
 
 export const ProductTable = ({ products }: { products: Product[] }) => {
-  return (
-    <div className="container mx-auto">
-      <DataTableProduct data={products || []} columns={columns} />
-    </div>
-  )
-}
+  return <DataTableProduct data={products || []} columns={columns} />;
+};
